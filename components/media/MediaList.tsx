f076b4de8e66855cb65c030ee02fdcfdfa8d3c9d
Dadu400 <daduka.xuxunashvili@gmail.com>
2024-09-06 17:16:52 +0400

    Small changes in styling

--- components/media/MediaList.tsx
+++ components/media/MediaList.tsx
@@ -1,38 +1,23 @@
 import Link from "next/link";
-import MediaCard from "./MediaCard";
+import MediaSwiper from "./MediaSwiper";
 
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import 'swiper/css/navigation';
+import 'swiper/css';
 
 
 function MediaList() {
 
-  const gridStyle = {
-    display: "grid",
-    gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
-    justifyContent: "center"
-  };
-
   return (
     <section className="w-full">
-      <div className="flex items-center justify-between m-6 border-l-4 pl-2 border-[#ff7f50]">
-        <span className="text-xl text-[#E2E2E9] uppercase">top 20 most popular</span>
+      <div className="flex items-center my-6 justify-between border-l-4 pl-2 border-[#ff7f50]">
+        <span className="text-xl text-[#E2E2E9] uppercase">Top 20 Most Popular</span>
         <Link href={"#"} className="text-[#ff7f50]">
-          See All</Link>
+          See All
+        </Link>
       </div> 
-      <div className="w-[100%] grid grid-cols-4 my-3 gap-4" style={gridStyle}>
-          <MediaCard />
-          <MediaCard />
-          <MediaCard />
-          <MediaCard />
-          <MediaCard />
-        </div>
-      <div className="flex items-center cursor-pointer justify-end">
-        <KeyboardArrowLeftIcon fontSize="large"/>
-        <KeyboardArrowRightIcon fontSize="large"/>
-      </div>
+      <MediaSwiper />
     </section>
-  )
+  );
 }
 
 export default MediaList;
