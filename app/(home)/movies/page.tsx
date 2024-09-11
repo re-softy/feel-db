01f123fac13a3bc9eeba3686fedbf14d4ff87d9a
Dadu400 <daduka.xuxunashvili@gmail.com>
2024-09-11 16:40:13 +0400

    Make Banner component reusable

--- app/(home)/movies/page.tsx
+++ app/(home)/movies/page.tsx
@@ -1,15 +1,15 @@
-import HomePageBanner from "@/components/banner/HomePageBanner";
+import Banner from "@/components/banner/Banner";
 import DashboardLayout from "../DashboardLayout";
 import AllContent from "@/components/media/AllContent";
 
 function page() {
   return (
     <DashboardLayout>
-        <main className="w-[80%] md:w-[76%] mx-auto py-[25px]">
-            <h1 className="text-3xl font-medium">Movies</h1>
-        <HomePageBanner/>
+      <main className="w-[90%] md:w-[85%] lg:w-[80%] xl:w-[75%] flex flex-col mx-auto py-[25px]">
+        <h1 className="text-3xl font-medium">Movies</h1>
+        <Banner isHomepage={false} newButtonText="Learn More" />
         <AllContent />
-        </main>
+      </main>
     </DashboardLayout>
   )
 }
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,13 @@
-import HomePageBanner from "@/components/banner/HomePageBanner";
+import Banner from "@/components/banner/Banner";
 import DashboardLayout from "./DashboardLayout";
 import MediaList from "@/components/media/MediaList";
 
 function page() {
   return (
     <DashboardLayout>
-      <main className="w-[90%] md:w-[85%] mx-auto py-[25px]">
-      <HomePageBanner />
-      <MediaList />
+      <main className="w-[90%] md:w-[85%] lg:w-[80%] xl:w-[75%] flex flex-col mx-auto py-[25px]">
+        <Banner isHomepage={true} />
+        <MediaList />
       </main>
     </DashboardLayout>
   )
