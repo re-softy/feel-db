5dec361397cb553483820d54523f9135bbeb4672
Dadu400 <daduka.xuxunashvili@gmail.com>
2024-09-18 15:08:34 +0400

    finished Profile info section

---
+++ app/(home)/profile/page.tsx
@@ -0,0 +1,12 @@
+import UserInfo from "@/components/profile/UserInfo";
+import DashboardLayout from "../DashboardLayout";
+
+function ProfilePage() {
+    return (
+        <DashboardLayout>
+            <UserInfo />
+        </DashboardLayout>
+    )
+}
+
+export default ProfilePage;
