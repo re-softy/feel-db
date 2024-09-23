0ce1bd5144bbcd0ef151cce101d064bd07654d41
Dadu400 <daduka.xuxunashvili@gmail.com>
2024-09-23 15:02:56 +0400

    Added example Dialog form for auth button

--- components/header/AuthButton.tsx
+++ components/header/AuthButton.tsx
@@ -1,9 +1,49 @@
 import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 function AuthButton() {
   return (
-    <PermIdentityIcon />
-  )
+    <Dialog>
+      <DialogTrigger asChild>
+        <button>
+          <PermIdentityIcon />
+        </button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when youre done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <label htmlFor="name" className="text-right">
+              Name
+            </label>
+            <input id="name" defaultValue="Pedro Duarte" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <label htmlFor="username" className="text-right">
+              Username
+            </label>
+            <input id="username" defaultValue="@peduarte" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <button type="submit">Save changes</button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
 
 export default AuthButton;
