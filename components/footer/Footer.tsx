540362daf3e4921d82815102644722f81d53bcc3
Dadu400 <daduka.xuxunashvili@gmail.com>
2024-10-28 16:50:03 +0400

    add responsive design to footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -5,20 +5,20 @@ import FooterSocials from "./FooterSocials";
 
 function Footer() {
   return (
-    <footer className="w-full px-6 py-4 gap-6 border-t-2 border-gray-700">
-      <div className="w-[90%] md:w-[85%] lg:w-[80%] xl:w-[75%] mx-auto flex flex-col mt-2 items-start justify-between">
-        <Image src={Logo} alt="logo" width={120} height={130} className="w-36" />
+    <footer className="w-full py-4 gap-6 border-t-2 border-grey">
+      <div className="w-[90%] md:w-[85%] lg:w-[80%] xl:w-[75%] mx-auto flex flex-col mt-2 items-center justify-between">
+        <Image src={Logo} alt="logo" width={120} height={130} className="w-36 self-start md:ml-10" />
         <FooterSocials />
       </div>
       <div className="flex flex-col items-center justify-center my-10 gap-2 md:flex-row md:gap-10">
-        <div className="flex items-center gap-8">
-        <Link href={'#'} className="text-md font-light">Terms and Conditions</Link>
-        <Link href={'#'} className="text-md font-light">Privacy Policy</Link>
+        <div className="flex items-center gap-4 md:gap-8">
+        <Link href={'#'} className="text-sm md:text-md lg:text-lg font-light">Terms and Conditions</Link>
+        <Link href={'#'} className="text-sm md:text-md lg:text-lg font-light">Privacy Policy</Link>
         </div>
         <span className="hidden md:block">&#8226;</span>
         <div className="flex items-center gap-4 md:gap-8">
         <p className="text-xs md:text-md lg:text-lg font-light">All rights reserved</p>
-        <p className="text-xs md:text-md font-light">Website contains 18+ content</p>
+        <p className="text-xs md:text-md lg:text-lg font-light">Website contains 18+ content</p>
         </div>
       </div>
     </footer>
--- components/footer/FooterSocials.tsx
+++ components/footer/FooterSocials.tsx
@@ -28,24 +28,24 @@ function FooterSocials() {
   return (
     <div className='w-[90%] flex flex-col gap-y-6 md:gap-y-0 md:flex-row items-start justify-between mt-8'>
       <div className='flex flex-col gap-3'>
-        <p className='uppercase text-xl'>Categories</p>
+        <p className='uppercase text-md md:text-lg lg:text-xl'>Categories</p>
         <ul>
-          <li className='text-lg mb-1'>Movies</li>
-          <li className='text-lg mb-1'>TV Shows</li>
-          <li className='text-lg mb-1'>Animation</li>
+          <li className='text-md md:text-lg lg:text-xl mb-1'>Movies</li>
+          <li className='text-md md:text-lg lg:text-xl mb-1'>TV Shows</li>
+          <li className='text-md md:text-lg lg:text-xl mb-1'>Animation</li>
         </ul>
       </div>
       <div className='flex flex-col gap-3'>
-        <p className='uppercase text-xl'>Sign Up</p>
+        <p className='uppercase text-md md:text-lg lg:text-xl'>Sign Up</p>
         <div className="relative w-full">
           <input placeholder="Enter Your Email"
-            className="w-full border-2 border-white outline-none text-white text-sm h-[44px] bg-black p-4"
+            className="w-[100%] border-[1px] border-[#E0E0E0] outline-none text-[#E0E0E0] text-sm h-[44px] bg-black p-4"
           />
           <ArrowForwardIcon className="absolute right-[10px] top-[50%] translate-y-[-50%] text-white cursor-pointer" />
         </div>
       </div>
       <div className='flex flex-col items-start gap-4'>
-        <p className='uppercase text-xl'>Social Media</p>
+        <p className='uppercase text-md md:text-lg lg:text-xl'>Social Media</p>
         <div className='flex w-full gap-4'>
           <Link href={'#'}>
             <FacebookIcon fontSize='large' />
@@ -62,13 +62,13 @@ function FooterSocials() {
         </div>
         <div className="flex mt-2">
           <DropdownMenu onOpenChange={toggleDropdown}>
-            <DropdownMenuTrigger className="text-white flex items-center uppercase gap-2 border-[1px] p-2 rounded-sm">
+            <DropdownMenuTrigger className="text-white flex items-center uppercase gap-2 border-[#E0E0E0] border-[1px] p-2 rounded-sm text-sm md:text-md lg:text-lg">
               <LanguageIcon fontSize='small'/>
               english
               {isDropdownOpen ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
             </DropdownMenuTrigger>
             <DropdownMenuContent>
-              <DropdownMenuItem>GEORGIAN</DropdownMenuItem>
+              <DropdownMenuItem className='text-sm md:text-md lg:text-lg'>GEORGIAN</DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
