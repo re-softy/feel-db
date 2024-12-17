af246cdeb611d5f5995ce1adad2451468f070b8b
Dadu400 <daduka.xuxunashvili@gmail.com>
2024-12-17 17:13:40 +0400

    WIP

--- app/(home)/profile/page.tsx
+++ app/(home)/profile/page.tsx
@@ -2,12 +2,14 @@ import UserInfo from "@/components/profile/UserInfo";
 import DashboardLayout from "../DashboardLayout";
 import UsersEmotion from "@/components/profile/UsersEmotion";
 import UsersFavorite from "@/components/profile/UsersFavorite";
+import UserAdminInfo from "@/components/profile/UserAdminInfo";
 
 function ProfilePage() {
     return (
         <DashboardLayout>
             <main className="w-[86%] flex flex-col mx-auto px-[1vw]">
-                <UserInfo />
+                {/*<UserInfo />*/}
+                <UserAdminInfo />
                 <UsersEmotion />
                 <UsersFavorite />
             </main>
