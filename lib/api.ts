145493950d21d126370a50d915bd689da8bee615
Dadu400 <daduka.xuxunashvili@gmail.com>
2025-02-04 23:11:39 +0400

    fetching with axios

--- lib/api.ts
+++ lib/api.ts
@@ -1,38 +1,49 @@
+import axios from "axios";
+
 export async function fetchMainPageData() {
-    try {
-      const response = await fetch(
-        `${process.env.API_BASE_URL}mainpage`,
-      );
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      return null;
-    }
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}mainpage`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
   }
+}
 
-  export async function fetchCollection() {
-    try {
-      const response = await fetch(
-        `${process.env.API_BASE_URL}collections`,
-      );
-      const data = await response.json();
-      return data.data;
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      return null;
-    }
+export async function fetchCollection() {
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}collections`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
   }
+}
 
-  export async function fetchDayTop() {
-    try {
-      const response = await fetch(
-        `${process.env.API_BASE_URL}daytop`,
-      );
-      const data = await response.json();
-      return data.data;
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      return null;
-    }
+export async function fetchDayTop() {
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}daytop`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
   }
+}
+
+export async function fetchSingleMedia(id: string) {
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}collections/${id}`
+    );
+    return response.data.collection;
+  } catch (error) {
+    console.error("Error fetching single media:", error);
+    return null;
+  }
+}
