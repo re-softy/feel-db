62f2e671a44bcf9d81a257df723b3590aa973ee0
Dadu400 <daduka.xuxunashvili@gmail.com>
2025-05-12 17:22:45 +0400

    remove old version of api endpoints

--- app/(home)/movies/page.tsx
+++ app/(home)/movies/page.tsx
@@ -2,28 +2,28 @@ import Banner from "@/components/banner/Banner";
 import DashboardLayout from "../DashboardLayout";
 import AllContent from "@/components/media/AllContent";
 import Pagination from "@/components/media/Pagination";
-import { fetchCollection } from "@/lib/api";
+// import { fetchCollection } from "@/lib/api";
 import { PaginatedResponse, PageProps } from "@/types/types";
 
 async function MoviesPage({ searchParams }: PageProps) {
   const currentPage = Math.max(1, Number(searchParams.page) || 1);
-  const mediaData: PaginatedResponse = await fetchCollection(currentPage);
+  // const mediaData: PaginatedResponse = await fetchCollection(currentPage);
 
-  const totalPages = mediaData?.hasNextPage ? currentPage + 1 : currentPage;
+  // const totalPages = mediaData?.hasNextPage ? currentPage + 1 : currentPage;
 
   return (
     <DashboardLayout>
       <main className="w-[90%] md:w-[85%] flex flex-col mx-auto my-4 gap-y-4">
         <h1 className="text-3xl font-medium">Movies</h1>
         <Banner />
-        <AllContent mediaItems={mediaData} />
+        {/* <AllContent mediaItems={mediaData} />
         {mediaData?.data?.length > 0 && (
           <Pagination 
             currentPage={currentPage} 
             totalPages={totalPages}
             searchParams={searchParams}
           />
-        )}
+        )} */}
       </main>
     </DashboardLayout>
   );
--- lib/api.ts
+++ lib/api.ts
@@ -13,25 +13,25 @@ export async function fetchMainPageData() {
   }
 }
 
-export async function fetchCollection(page: number = 1) {
-  try {
-    const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}collections?page=${page}`
-    );
+// export async function fetchCollection(page: number = 1) {
+//   try {
+//     const response = await axios.get(
+//       `${process.env.NEXT_PUBLIC_API_BASE_URL}collections?page=${page}`
+//     );
     
-    const nextPageCheck = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}collections?page=${page + 1}`
-    );
+//     const nextPageCheck = await axios.get(
+//       `${process.env.NEXT_PUBLIC_API_BASE_URL}collections?page=${page + 1}`
+//     );
     
-    return {
-      ...response.data,
-      hasNextPage: nextPageCheck.data.data.length > 0
-    };
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return null;
-  }
-}
+//     return {
+//       ...response.data,
+//       hasNextPage: nextPageCheck.data.data.length > 0
+//     };
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//     return null;
+//   }
+// }
 
 // export async function fetchDayTop() {
 //   try {
@@ -46,17 +46,17 @@ export async function fetchCollection(page: number = 1) {
 //   }
 // }
 
-export async function fetchSingleMedia(id: string) {
-  try {
-    const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}collections/${id}`
-    );
-    return response.data.collection;
-  } catch (error) {
-    console.error("Error fetching single media:", error);
-    return null;
-  }
-}
+// export async function fetchSingleMedia(id: string) {
+//   try {
+//     const response = await axios.get(
+//       `${process.env.NEXT_PUBLIC_API_BASE_URL}collections/${id}`
+//     );
+//     return response.data.collection;
+//   } catch (error) {
+//     console.error("Error fetching single media:", error);
+//     return null;
+//   }
+// }
 
 export async function fetchUserData(authToken?: string) {
   try {
