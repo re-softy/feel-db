54901ec465a01238fba1e98296fef847a73726f2
Dadu400 <daduka.xuxunashvili@gmail.com>
2025-05-31 21:12:32 +0400

    remove unnecessary components from pages

--- app/(home)/tvseries/[id]/page.tsx
+++ app/(home)/tvseries/[id]/page.tsx
@@ -1,28 +1,16 @@
-import DashboardLayout from "../../DashboardLayout";
+import DashboardLayout from "@/app/(home)/DashboardLayout";
 import SingleMediaDescription from "@/components/singlemedia/SingleMediaDescription";
 import AddMediaReview from "@/components/singlemedia/AddMediaReview";
-import AllComments from "@/components/singlemedia/AllComments";
 import { fetchSingleMedia } from "@/lib/api";
 
 async function SingleMoviePage({ params }: { params: { id: string } }) {
   const mediaData = await fetchSingleMedia(params.id);
-
-  // if (!mediaData) {
-  //   return (
-  //     <DashboardLayout>
-  //       <main className="w-[86%] flex flex-col mx-auto px-[1vw] pb-8">
-  //         <h1 className="text-3xl font-medium text-red-500">Media Not Found</h1>
-  //       </main>
-  //     </DashboardLayout>
-  //   );
-  // }
 
   return (
     <DashboardLayout>
       <main className="w-[86%] flex flex-col mx-auto px-[1vw] pb-8">
         <SingleMediaDescription media={mediaData} />
         <AddMediaReview media={mediaData} />
-        <AllComments />
       </main>
     </DashboardLayout>
   );
